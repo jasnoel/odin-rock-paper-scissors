0e08c90d431f866f396b1c6bd5bd28b19062c998
jasnoel <jason.noel.imp@gmail.com>
2022-03-22 01:03:01 +0100

    added computerPlay function

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hello world!")
+let computerPlay = () => {
+    let nb = Math.floor(Math.random() * 3)
+    console.log(nb)
+    if (nb == 0) {
+        return "Rock"
+    } else if (nb == 1) {
+        return "Paper"
+    } else {
+        return "Scissors"
+    }
+}
+
+console.log(computerPlay())
