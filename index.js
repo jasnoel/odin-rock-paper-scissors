7ad18dbc1757661cb0a6086fd24ff79e805e02ce
jasnoel <jason.noel.imp@gmail.com>
2022-03-24 14:19:48 +0100

    deleted game function added UI

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const MAX_SCORE = 5;
+
 const computerPlay = () => {
     let nb = Math.floor(Math.random() * 3)
     if (nb == 0) {
@@ -48,30 +50,54 @@ const round = (playerSelection, computerSelection) => {
     playerSelection = playerSelection.trim().toLowerCase()
     computerSelection = computerSelection.trim().toLowerCase()
     let result = battle(playerSelection, computerSelection);
-    
+  
     return result
-} 
+}
 
-const game = () => {
-    let playerScore = 0;
-    let computerScore = 0;
-    let historic = [];
-    while (playerScore < 3 && computerScore < 3) {
-        let playerPlay = prompt("Enter rock paper or scissors").trim().toLocaleLowerCase();
-        let result = round(playerPlay, computerPlay())
-        if (result.trim().toLocaleLowerCase().includes('win')) {
-            playerScore += 1;
-        } else if (result.trim().toLocaleLowerCase().includes('loose')) {
-            computerScore += 1;
-        }
-        historic.push(result);
-        console.log(result);
-    }
-    if (playerScore >= 3) {
-        console.log("**** YOU WON THE BEST OF 5 :D ****");
-    } else if (computerScore >= 3) {
-        console.log("**** YOU LOST THE BEST OF 5 :c ****")
+const updateScores = (result) => {
+    if (result.trim().toLocaleLowerCase().includes('win')) {
+        playerScore += 1;
+    } else if (result.trim().toLocaleLowerCase().includes('loose')) {
+        computerScore += 1;
     }
 }
 
-game()
+const checkVictory = () => {
+    if (playerScore >= MAX_SCORE || computerScore >= MAX_SCORE) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let playerScore = 0;
+let computerScore = 0;
+
+const output = document.querySelector(".output h3");
+const scores = document.querySelector(".output h2");
+const controls = document.querySelector(".controls");
+
+const playAgain = document.querySelector(".play-again");
+playAgain.addEventListener('click', () => {
+    controls.classList.remove("inactive");
+    playAgain.classList.add("inactive");
+    playerScore = 0;
+    computerScore = 0;
+    output.textContent = "Pick your play !"
+    scores.textContent = `${playerScore} | ${computerScore}`
+})
+
+const buttons = document.getElementsByClassName('btn');
+Array.from(buttons).forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        let result = round(e.target.classList[1], computerPlay());
+        updateScores(result);
+        if (checkVictory()) {
+            result = playerScore > computerScore ? "You won !" : "You lost :/"
+            controls.classList.add("inactive");
+            playAgain.classList.remove("inactive");
+        }
+        output.textContent = result;
+        scores.textContent = `${playerScore} | ${computerScore}`
+    })
+});
